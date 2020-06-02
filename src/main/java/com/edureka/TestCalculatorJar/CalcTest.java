e20955f4d268b5b6351056f3356590929771d038
prashant <pbeniwal@yahoo.com>
2020-06-02 12:24:32 +0530

    up

--- src/main/java/com/edureka/TestCalculatorJar/CalcTest.java
+++ src/main/java/com/edureka/TestCalculatorJar/CalcTest.java
@@ -1,15 +1,11 @@
 package com.edureka.TestCalculatorJar;
 
-import java.io.FileInputStream;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 import java.io.IOException;
 
 
@@ -20,21 +16,13 @@ public class CalcTest
 		
 	    WebDriver driver;
 	    
-	    //Properties prop = new Properties();
-	    //FileInputStream f = new FileInputStream("./data.properties");
-	    //prop.load(f);
-	    //String myIP = prop.getProperty("public_ip");
-	    //String myPort = prop.getProperty("tomcat_port");
-	    //String myAppName = prop.getProperty("app_name");
-	    //String myURL = "http://" + myIP + ":" + myPort + "/" + myAppName;
 	    String myURL = "http://130.211.229.175:9090/calculator/";
 	    FirefoxOptions options = new FirefoxOptions();
         
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
 	    options.setCapability("requireWindowFocus", true);
         String mygecko=System.getenv("HOME") + "/Downloads/geckodriver";
-        //String mygecko= prop.getProperty("webdriver_path") + "geckodriver";
-
+        
         System.setProperty("webdriver.gecko.driver",mygecko);
         
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
@@ -50,9 +38,7 @@ public class CalcTest
         
         Thread.sleep(5000);
         
-	    //String text = prop.getProperty("exp_add_text");
 	    String text = "Addition";
-	    //int len = text.length();
 
         String bodyText = driver.findElement(By.xpath("/html/body")).getText();
         System.out.println(bodyText);
@@ -68,11 +54,9 @@ public class CalcTest
 	    
 	    System.out.println(bodyText);
 	    
-	    //Assert.assertEquals(bodyText.substring(0, len),text);
-	    
 	    Assert.assertTrue(bodyText.contains(text), text + " not found!" );
 	    
-	    Assert.assertTrue(bodyText.contains(text), "50 not found!" );
+	    Assert.assertTrue(bodyText.contains("50"), "50 not found!" );
 	    
 	    Thread.sleep(5000);
 	    
