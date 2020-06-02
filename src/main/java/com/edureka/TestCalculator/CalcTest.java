bee0150066e24abfdb56bff5203661f82ee8e77d
prashant <pbeniwal@yahoo.com>
2020-06-02 11:02:37 +0530

    Up

--- src/main/java/com/edureka/TestCalculator/CalcTest.java
+++ src/main/java/com/edureka/TestCalculator/CalcTest.java
@@ -58,6 +58,8 @@ public class CalcTest
 	    driver.findElement(By.xpath("/html/body/form/input[1]")).sendKeys("12");
 	    driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("38");
         
+	    driver.findElement(By.name("r1")).click();
+	    
 	    driver.findElement(By.xpath("/html/body/form/input[3]")).click();
 	    Thread.sleep(5000);
 	    
